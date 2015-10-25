0c4061d75abcc3155679e2ee86dbdd398cef741d
James Hu <hello@james.hu>
2015-10-25 13:34:15 -0700

    Add iplan

---
+++ node/iplan/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var handlebars = require('express-handlebars');
+var app = express();
+
+app.engine('handlebars', handlebars({ defaultLayout: 'application' }));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
